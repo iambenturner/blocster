3d63427a11b2bf82dfd4685b7170d1a30b8bf4bd
Ben Turner <benwestur@gmail.com>
2016-11-21 11:58:59 -0500

    bloctser files

---
+++ script.js
@@ -0,0 +1,46 @@
+var main = function() {
+
+
+$('#home').click(function(){
+
+  window.location = "index.html"
+
+});
+$('#business').click(function(){
+
+  window.location = "business.html"
+
+});
+
+$('#crime').click(function(){
+
+  window.location = "crime.html"
+
+});
+
+$('#education').click(function(){
+
+  window.location = "education.html"
+
+});
+
+$('#politics').click(function(){
+
+  window.location = "politics.html"
+
+});
+
+$('#sports').click(function(){
+
+  window.location = "sports.html"
+
+});
+
+
+
+};
+
+
+
+
+$(document).ready(main);
